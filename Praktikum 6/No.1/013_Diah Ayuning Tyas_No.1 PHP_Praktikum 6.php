a0c07a3ab4da5346c712db2c431c41116b2ffecf
diahayuningtyas <90167777+diahayuningtyas@users.noreply.github.com>
2022-10-14 22:21:55 +0700

    Add files via upload

---
+++ Praktikum 6/No.1/013_Diah Ayuning Tyas_No.1 PHP_Praktikum 6.php
@@ -0,0 +1,39 @@
+<!-- Diah Ayuning Tyas - 21091397013 -->
+
+<?php 
+
+require_once '../No1/abstract.php';
+
+class Truck extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+    
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+        
+        return ceil($fuel /= $trip);
+    }
+}
+
+class RiverBarge extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+
+        return ceil($fuel /= $trip);
+    }
+}
+$truck = new Truck(15000, 'Truk');
+$riverBarge = new RiverBarge(20000, 'Perahu');
---
+++ Praktikum 6/No.3/013_Diah Ayuning Tyas_No.3 Interface_Praktikum 6.php
@@ -0,0 +1,13 @@
+<!-- Diah Ayuning Tyas - 21091397013 -->
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
